fe36663c0bb915e015b0d8f4c00d6e8e664eace5
StubarevDmitry <d.stubarev@g.nsu.ru>
2025-10-01 22:23:09 +0700

    Created rest api и websocket

---
+++ src/features/chat/api/chatApi.tsx
@@ -0,0 +1,10 @@
+import { baseApi } from "../../../shared/api/http/baseApi";
+
+export const chatApi = {
+  // getMessages: (chatId: string): Promise<Message[]> =>
+  //   baseApi.get(`/chats/${chatId}/messages`),
+  // sendMessage: (chatId: string, content: string): Promise<Message> =>
+  //   baseApi.post(`/chats/${chatId}/messages`, { content }),
+  // createChat: (participants: string[]): Promise<Chat> =>
+  //   baseApi.post("/chats", { participants }),
+};
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,7 @@
 import Button from "../shared/atoms/buttons/Button";
 
 import IconButton from "../shared/atoms/buttons/IconButton";
-import Header from "../components/Header"
+import Header from "../components/Header";
 import buttonClasses from "../shared/atoms/buttons/Button.module.css";
 import MainPageButtons from "../components/MainPageButtons";
 import LinkButton from "../shared/atoms/buttons/LinkButton";
@@ -10,33 +10,40 @@ import Icon from "../shared/atoms/icons/Icon";
 export function MainPage() {
   return (
     <div className={"main-page-container"}>
-        <Header />
-        <LinkButton label={<Label text={"Перейди по ссылке!!!"} color={"#fff"}/>} url={"https://ai.nsu.ru/projects/dovcom/time_entries"}/>
-        <main>
-            <MainPageButtons />
-            <Button className={buttonClasses.defaultButtonBlue}
-                    label={<Label text={"Кнопка"}/>}
-                    width="200px" />
-          <Button className={buttonClasses.defaultButtonOrange}
-            label={<Label text={"Войти"}/>}
-            icon={<Icon path="/icons/login.svg"/>}
-            labelPosition="right"
-          />
-          <IconButton
-              className={`${buttonClasses.defaultButtonOrange}`}
-              icon={<Icon path="/icons/login.svg" size={"40px"}/> }
-            size="70px"
-            onClick={() => console.log("Clicked!")}
-          />
-
-          <Button
-              className={buttonClasses.defaultWarningButton}
-            label={<Label text={"Опасное действие"} />}
-          />
-          <Button
-              className={buttonClasses.defaultButtonBlue} label={<Label text={"Недоступно"} />} disabled={true}
-          />
-        </main>
+      <Header />
+      <LinkButton
+        label={<Label text={"Перейди по ссылке!!!"} color={"#fff"} />}
+        url={"https://ai.nsu.ru/projects/dovcom/time_entries"}
+      />
+      <main>
+        <MainPageButtons />
+        <Button
+          className={buttonClasses.defaultButtonBlue}
+          label={<Label text={"Кнопка"} />}
+          width="200px"
+        />
+        <Button
+          className={buttonClasses.defaultButtonOrange}
+          label={<Label text={"Войти"} />}
+          icon={<Icon path="/icons/login.svg" />}
+          labelPosition="right"
+        />
+        <IconButton
+          className={`${buttonClasses.defaultButtonOrange}`}
+          icon={<Icon path="/icons/login.svg" size={"40px"} />}
+          size="70px"
+          onClick={() => console.log("Clicked!")}
+        />
+        <Button
+          className={buttonClasses.defaultWarningButton}
+          label={<Label text={"Опасное действие"} />}
+        />
+        <Button
+          className={buttonClasses.defaultButtonBlue}
+          label={<Label text={"Недоступно"} />}
+          disabled={true}
+        />
+      </main>
     </div>
   );
 }
