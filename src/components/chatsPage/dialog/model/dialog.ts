6655287fc833da785b9bad32e5be410292524473
stuba <d.stubarev@g.nsu.ru>
2025-12-22 21:28:56 +0700

    fix: log fix

--- src/components/chatsPage/dialog/model/dialog.ts
+++ src/components/chatsPage/dialog/model/dialog.ts
@@ -28,7 +28,7 @@ export const useDialog = (selectedChat: Chat) => {
     const signalStore = useSignalRStore.getState();
     console.log("я не зайду сюда дважды")
     signalStore.subscribe("ReceiveNotification", (response: any) => {
-      console.log("это id из response", response.chatId);
+      console.log("это id из response", response.ChatId);
       console.log("это id чата", selectedChat.id);
       if(response.ChatId === selectedChat.id){
         const newMessage: MessageEntity = {
